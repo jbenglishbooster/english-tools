418cee48daf30864da08a6eba99dc6771914aa9d
jbenglishbooster <jbenglishbooster@gmail.com>
2026-08-04 14:34:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,9 +24,38 @@ const words = [
 "Basic - Simple and important",
 "Believe - To think something is true",
 "Benefit - Something helpful",
-"Bright - Full of light"
+"Bright - Full of light",
 ];
-
+"Air - The mixture of gases we breathe",
+"Airport - A place where airplanes land and take off",
+"Animal - A living creature",
+"Apple - A sweet fruit",
+"Apply - To make a request",
+"April - The fourth month of the year",
+"Area - A particular place",
+"Army - A group of soldiers",
+"Around - On every side",
+"Arrive - To reach a place",
+"Ask - To request information",
+"Attack - To try to hurt",
+"August - The eighth month",
+"Aunt - Sister of your father or mother",
+"Autumn - The season after summer",
+"Baby - A very young child",
+"Back - The rear part",
+"Bad - Not good",
+"Bag - A container for carrying things",
+"Ball - A round object",
+"Banana - A yellow fruit",
+"Bank - A place to keep money",
+"Bathroom - A room for washing",
+"Beach - Sandy land beside the sea",
+"Beautiful - Very attractive",
+"Because - For the reason that",
+"Become - To begin to be",
+"Bed - Furniture for sleeping",
+"Before - Earlier than",
+"Begin - To start"
 function showWord() {
   const random = words[Math.floor(Math.random() * words.length)];
   document.getElementById("result").innerHTML =
